08c5358d69defe9692f9e5239b42f99b51ed2255
ravipatel <102030@@>
2019-04-20 17:44:13 +0530

    init commit

---
+++ appintro/consumer-proguard-rules.pro
@@ -0,0 +1,1 @@
+-keep class devs.paolorotolo.** {*;}
---
+++ example/src/main/java/com/amqtech/opensource/appintroexample/ui/mainTabs/intro/CustomLayoutIntro.java
@@ -0,0 +1,40 @@
+package com.amqtech.opensource.appintroexample.ui.mainTabs.intro;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.amqtech.opensource.appintroexample.util.SampleSlide;
+import com.devs.nikintro.AppIntro;
+import com.devs.appintroexample.R;
+
+/**
+ * Created by andrew on 11/17/16.
+ */
+
+public class CustomLayoutIntro extends AppIntro {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addSlide(SampleSlide.newInstance(R.layout.intro_custom_layout1));
+        addSlide(SampleSlide.newInstance(R.layout.intro_custom_layout2));
+        addSlide(SampleSlide.newInstance(R.layout.intro_custom_layout3));
+        addSlide(SampleSlide.newInstance(R.layout.intro_custom_layout4));
+
+        setProgressIndicator();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
+}
